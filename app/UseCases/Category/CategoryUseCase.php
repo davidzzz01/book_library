af0fde0779f2d8e723aa69d12bb46ddf41d82c4c
David Dantas <davidjose001@outlook.com>
2025-06-14 19:01:03 -0300

    Update CategoryUseCase.php

--- app/UseCases/Category/CategoryUseCase.php
+++ app/UseCases/Category/CategoryUseCase.php
@@ -10,31 +10,31 @@ class CategoryUseCase
         private CategoryRepository $categoryRepository
     ) {}
 
-    // Listar todas categorias
+    
     public function getAll()
     {
         return $this->categoryRepository->all();
     }
 
-    // Buscar categoria por ID
+    
     public function getById(int $id)
     {
         return $this->categoryRepository->find($id);
     }
 
-    // Criar nova categoria
+
     public function create(array $data)
     {
         return $this->categoryRepository->create($data);
     }
 
-    // Atualizar categoria
+    
     public function update(int $id, array $data)
     {
         return $this->categoryRepository->update($id, $data);
     }
 
-    // Deletar categoria
+    
     public function delete(int $id)
     {
         return $this->categoryRepository->delete($id);
